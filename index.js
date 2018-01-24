c60b1e450e51ec917b3772b1eac1576ea7e49079
wangliansheng <8863704@qq.com>
2018-01-24 11:28:31 +0800

    精简代码

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
-//var fs= ('fs');
 var http=require('http');
 var router= require('router');
 
 http.createServer(function(req,res){
-    router(req,res,'html')
-
-    // var url =req.url;
-    // var pathextname=path.parse(urlurl.parse(url).pathname).ext;
-    // if(url=='/'){ url='/index.html'; };
-    // if(url!='/favicon.ico'){
-    //     fs.readFile('html'+url,function(error,data){
-    //             //console.log(url);
-    //             if(error){
-    //                 fs.readFile("html/404.html",function(error,data404){
-    //                     res.writeHead(404,{"Content-Type":"text/html;charset='utf-8'"});
-    //                     res.write(data404);
-    //                     res.end();
-    //                 });
-                  
-    //             }else{
-    //                     getmm(fs,pathextname,function(extn){
-    //                         console.log(extn);
-    //                         res.writeHead(200,{"Content-type":""+extn+";charset='utf-8'"});
-    //                         res.write(data);
-    //                         res.end();
-    //                     });
-                    
-                    
-    //             }
-                
-    //     })
-
-    // }
-    
-    
-    
+    router(req,res,'html')   
 }).listen(8001);
